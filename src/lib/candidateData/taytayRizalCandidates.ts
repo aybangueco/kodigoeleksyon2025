23aeda6e3b412986eae11c39adb33d6e7c9cbc85
Jhoenil Wahid <aljhoenilw@gmail.com>
2025-04-06 11:40:42 +0800

    feat: update Taytay candidates

--- src/lib/candidateData/taytayRizalCandidates.ts
+++ src/lib/candidateData/taytayRizalCandidates.ts
@@ -4,8 +4,8 @@ import { Candidate } from '../positions';
 export const taytayRizalHouseRepCandidates: Candidate[] = [
   { id: "taytay-hr1", name: "GALAS, ANTON (POR)", party: "PARTIDO OBRERO RIZAL" },
   { id: "taytay-hr2", name: "NARCISO, JAY (PDSP)", party: "PARTIDO DEMOKRATIKO PILIPINO" },
-  { id: "taytay-hr3", name: "VILLAFUERTE, RITA JR. (IND)", party: "INDEPENDENT" },
-  { id: "taytay-hr4", name: "YNARES, MA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-hr3", name: "VPALLASIGUE, KUYA JB (IND)", party: "INDEPENDENT" },
+  { id: "taytay-hr4", name: "YNARES, MIA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
 ];
 
 export const taytayRizalProvincialGovernorCandidates: Candidate[] = [
@@ -16,21 +16,22 @@ export const taytayRizalProvincialGovernorCandidates: Candidate[] = [
 ];
 
 export const taytayRizalProvincialViceGovernorCandidates: Candidate[] = [
-  { id: "taytay-pvg1", name: "BADAJOS, MIKE (PDSP)", party: "PARTIDO DEMOKRATIKO SOSYALISTA NG PILIPINAS" },
-  { id: "taytay-pvg2", name: "GATBAYATAN, PINENG (IND)", party: "INDEPENDENT" },
-  { id: "taytay-pvg3", name: "MANUEL, REYNALDO (IND)", party: "INDEPENDENT" },
+  { id: "taytay-pg1", name: "ACOL, GLENN (IND)", party: "INDEPENDENT" },
+  { id: "taytay-pg2", name: "PEREZ, RONALD (IND)", party: "INDEPENDENT" },
+  { id: "taytay-pg3", name: "VELASCO, JOSE (IND)", party: "INDEPENDENT" },
+  { id: "taytay-pg4", name: "YNARES, NINA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
 ];
 
 export const taytayRizalBoardMemberCandidates: Candidate[] = [
-  { id: "taytay-bm1", name: "AGALLO, BROTHER BOY (IND)", party: "INDEPENDENT" },
-  { id: "taytay-bm2", name: "ALARCON, JESTON (IND)", party: "INDEPENDENT" },
+  { id: "taytay-bm1", name: "AGPALO, BROTHER BOY (IND)", party: "INDEPENDENT" },
+  { id: "taytay-bm2", name: "ALARCON, JESTONI (IND)", party: "INDEPENDENT" },
   { id: "taytay-bm3", name: "CRUZ, EDWIN (IND)", party: "INDEPENDENT" },
-  { id: "taytay-bm4", name: "CRUZ, POPPO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-bm4", name: "CRUZ, PAPOO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "taytay-bm5", name: "DELA ROSA, LOUIE (IND)", party: "INDEPENDENT" },
   { id: "taytay-bm6", name: "HUANG, JOSEPH (IND)", party: "INDEPENDENT" },
-  { id: "taytay-bm7", name: "LEGASPI, CARLO, KAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-bm8", name: "MAMAMA, JAZZ (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-bm9", name: "TIAMSOY, PATINDAY (NP)", party: "NACIONALISTA PARTY" },
+  { id: "taytay-bm7", name: "ILAGAN-CONDE, KAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-bm8", name: "MAMBA, JAKZ (IND)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-bm9", name: "TIAMSON, PATNUBAY (NP)", party: "NACIONALISTA PARTY" },
 ];
 
 export const taytayRizalMayorCandidates: Candidate[] = [
@@ -41,8 +42,8 @@ export const taytayRizalMayorCandidates: Candidate[] = [
 
 export const taytayRizalViceMayorCandidates: Candidate[] = [
   { id: "taytay-vm1", name: "ANGELES, FILIPINA (IND)", party: "INDEPENDENT" },
-  { id: "taytay-vm2", name: "CARITAC, JV (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-vm3", name: "CABIRAI, PA (LAKAS)", party: "LAKAS-CMD" },
+  { id: "taytay-vm2", name: "CABITAC, JV (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-vm3", name: "CABRAL, PIA (LAKAS)", party: "LAKAS-CMD" },
   { id: "taytay-vm4", name: "VILLANUEVA, JECA (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
 ];
 
@@ -50,33 +51,33 @@ export const taytayRizalCouncilorCandidates: Candidate[] = [
   { id: "taytay-c1", name: "ALCANTARA, PATRICK (IND)", party: "INDEPENDENT" },
   { id: "taytay-c2", name: "ANGKIAS, MILLET (IND)", party: "INDEPENDENT" },
   { id: "taytay-c3", name: "BACCAY, NORA (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c4", name: "BERMUDO, HELEN (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c4", name: "BERBAÑO, HELEN (IND)", party: "INDEPENDENT" },
   { id: "taytay-c5", name: "BERMUNDO, MITCH (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
-  { id: "taytay-c6", name: "BONILLO, OFELIA (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c6", name: "BONILLO, OTILIA (IND)", party: "INDEPENDENT" },
   { id: "taytay-c7", name: "CALDERON, ARCHIE (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
   { id: "taytay-c8", name: "CALDERON, JOAN (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "taytay-c9", name: "CAYTON, TEGAN (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
-  { id: "taytay-c10", name: "CELEBRAR, GINO (AKSYON)", party: "AKSYON DEMOKRATIKO" },
+  { id: "taytay-c10", name: "CELERIAN, GINO (AKSYON)", party: "AKSYON DEMOKRATIKO" },
   { id: "taytay-c11", name: "CORTEZ, CAI (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "taytay-c12", name: "CRUZ, GERALD (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
-  { id: "taytay-c13", name: "CRUZ, TOBY (AKSYN)", party: "AKSYON DEMOKRATIKO" },
-  { id: "taytay-c14", name: "DE LEON, FRONEL (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-c15", name: "DIEGO, FERMIN (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c16", name: "DING, TAY FALKON (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-c17", name: "ESGUERRA, DINO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-c13", name: "CRUZ, TOBIT (AKBYN)", party: "AKBAYAN" },
+  { id: "taytay-c14", name: "DE LEON, RHONIEL (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c15", name: "DIEGO, ERMIN (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c16", name: "DIÑO, TAK FAELDON (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-c17", name: "ESGUERRA, KIKO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "taytay-c18", name: "FLORENDO, RANILO (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c19", name: "FRANCISCO, GRACE (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "taytay-c19", name: "FRANCISCO, GERICK (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
   { id: "taytay-c20", name: "GONZAGA, MANET (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
-  { id: "taytay-c21", name: "GURABOT, BONGUT (LAKAS)", party: "LAKAS-CMD" },
-  { id: "taytay-c22", name: "LAQUIO, KOYA COL. (AKSYON)", party: "AKSYON DEMOKRATIKO" },
-  { id: "taytay-c23", name: "LEONARDO, BONRAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-c21", name: "JURADO, BONGGIT (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "taytay-c22", name: "LAGUNA, KUYA COL. JUN (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c23", name: "LEONARDO, BOKNAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "taytay-c24", name: "LOPEZ, ALLYSON (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
-  { id: "taytay-c25", name: "MANNING, ARKY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-c26", name: "MERCADO, DINAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-c27", name: "VILLANUEVA, VICTORIA (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c28", name: "PERSIA, CHESCA (PRP)", party: "PEOPLE'S REFORM PARTY" },
+  { id: "taytay-c25", name: "MANNING, ARKY (PDPLBN))", party: "PARTIDO DEMOKRATIKO PILIPINO LAKAS NG BAYAN" },
+  { id: "taytay-c26", name: "MERCADO, NINAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-c27", name: "PALENZUELA, VICTORIA (IND)", party: "INDEPENDENT" },
+  { id: "taytay-c28", name: "PERSIA, CHESCA (KNP)", party: "KATIPUNAN NG NAGKAKAISANG PILIPINO" },
   { id: "taytay-c29", name: "SANTOS, MA. TERESA (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c30", name: "SORBA, MIKE (IND)", party: "INDEPENDENT" },
-  { id: "taytay-c31", name: "VIDAL, VJ (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "taytay-c32", name: "VICTOR, ELGIN (PROBIDO)", party: "PROBIDO" }
+  { id: "taytay-c30", name: "SORELA, MIKE (PROMDI)", party: "PROMDI" },
+  { id: "taytay-c31", name: "VALERA, JV (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "taytay-c32", name: "VICTOR, ELJUN (PROMDI)", party: "PROMDI" }
 ];
