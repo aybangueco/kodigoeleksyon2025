f27b511ca355ad9da74436402827d10d61516856
Jhoenil Wahid <aljhoenilw@gmail.com>
2025-04-06 11:25:25 +0800

    feat: update cainta rizal candidates

--- src/lib/candidateData/caintaRizalCandidates.ts
+++ src/lib/candidateData/caintaRizalCandidates.ts
@@ -4,8 +4,8 @@ import { Candidate } from '../positions';
 export const caintaRizalHouseRepCandidates: Candidate[] = [
   { id: "cainta-hr1", name: "GALAS, ANTON (POR)", party: "PARTIDO OBRERO RIZAL" },
   { id: "cainta-hr2", name: "NARCISO, JAY (PDP)", party: "PARTIDO DEMOKRATIKO PILIPINO" },
-  { id: "cainta-hr3", name: "VILLAFUERTE, RITA JR. (IND)", party: "INDEPENDENT" },
-  { id: "cainta-hr4", name: "YNARES, MA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-hr3", name: "PALLASIGUE, KUYA JB (IND)", party: "INDEPENDENT" },
+  { id: "cainta-hr4", name: "YNARES, MIA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
 ];
 
 export const caintaRizalProvincialGovernorCandidates: Candidate[] = [
@@ -17,20 +17,22 @@ export const caintaRizalProvincialGovernorCandidates: Candidate[] = [
 
 export const caintaRizalProvincialViceGovernorCandidates: Candidate[] = [
   { id: "cainta-pvg1", name: "BADAJOS, MIKE (PDSP)", party: "PARTIDO DEMOKRATIKO SOSYALISTA NG PILIPINAS" },
-  { id: "cainta-pvg2", name: "GATBAYATAN, PINENG (IND)", party: "INDEPENDENT" },
-  { id: "cainta-pvg3", name: "MANUEL, REYNALDO (IND)", party: "INDEPENDENT" },
+  { id: "cainta-pvg2", name: "BAUTISTA, KUYA JOJO (IND)", party: "INDEPENDENT" },
+  { id: "cainta-pvg3", name: "GATLABAYAN, PINING (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-pvg4", name: "GUILLERGAN, GHERRY (IND)", party: "INDEPENDENT" },
+  { id: "cainta-pvg5", name: "MANUEL, REYNALDO (IND)", party: "INDEPENDENT" },
 ];
 
 export const caintaRizalBoardMemberCandidates: Candidate[] = [
-  { id: "cainta-bm1", name: "AGALLO, BROTHER BOY (IND)", party: "INDEPENDENT" },
-  { id: "cainta-bm2", name: "ALARCON, JESTON (IND)", party: "INDEPENDENT" },
+  { id: "cainta-bm1", name: "AGPALO, BROTHER BOY (IND)", party: "INDEPENDENT" },
+  { id: "cainta-bm2", name: "ALARCON, JESTONI (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "cainta-bm3", name: "CRUZ, EDWIN (IND)", party: "INDEPENDENT" },
-  { id: "cainta-bm4", name: "CRUZ, POPPO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-bm4", name: "CRUZ, PAPPO (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "cainta-bm5", name: "DELA ROSA, LOUIE (IND)", party: "INDEPENDENT" },
   { id: "cainta-bm6", name: "HUANG, JOSEPH (IND)", party: "INDEPENDENT" },
-  { id: "cainta-bm7", name: "LEGASPI, CARLO, KAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-bm8", name: "MAMAMA, JAZZ (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-bm9", name: "TIAMSOY, PATINDAY (IND)", party: "INDEPENDENT" },
+  { id: "cainta-bm7", name: "ILAGAN-CONDE, KAY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-bm8", name: "MAMBA, JAKZ (IND)", party: "INDEPENDENT" },
+  { id: "cainta-bm9", name: "IAMSON, PATNUBAY (NP)", party: "NACIONALISTA PARTY" },
 ];
 
 export const caintaRizalMayorCandidates: Candidate[] = [
@@ -43,16 +45,16 @@ export const caintaRizalViceMayorCandidates: Candidate[] = [
 ];
 
 export const caintaRizalCouncilorCandidates: Candidate[] = [
-  { id: "cainta-c1", name: "ALMODOVA, PUTA CHARLES (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c1", name: "ALMODOVA, PUTA CHARLES (AKAY)", party: "AKAY" },
   { id: "cainta-c2", name: "BAJAS, BRADLEY (IND)", party: "INDEPENDENT" },
   { id: "cainta-c3", name: "CASIMINTAN, DADDY LLOYD (IND)", party: "INDEPENDENT" },
   { id: "cainta-c4", name: "FELIX, KAP WALLY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "cainta-c5", name: "GARCIA, SHARON (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "cainta-c6", name: "JACOB, MANNY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-c7", name: "KIRMA, ASA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-c8", name: "MALCOM, SNOOKY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c7", name: "KIRMA, AISA (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c8", name: "MALICDEM, SNOOKY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
   { id: "cainta-c9", name: "MELANO, ARCHIE (IND)", party: "INDEPENDENT" },
-  { id: "cainta-c10", name: "SAN JUAN, BOMBOY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-c11", name: "SAURO, BUDDY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
-  { id: "cainta-c12", name: "TAJUNA, ERZING (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c10", name: "SAN JUAN, ROMBOY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c11", name: "SAURO, BUBOY (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
+  { id: "cainta-c12", name: "TAJUNA, EKENG (NPC)", party: "NATIONALIST PEOPLE'S COALITION" },
 ];
