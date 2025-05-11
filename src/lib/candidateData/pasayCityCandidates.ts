a80181b4619c94bfe95d37d03995756736133ae8
JHNLWHD <aljhoenilw@gmail.com>
2025-05-11 10:10:15 +0800

    feat: update pasay city candidates

--- src/lib/candidateData/pasayCityCandidates.ts
+++ src/lib/candidateData/pasayCityCandidates.ts
@@ -7,44 +7,46 @@ export const pasayCityHouseRepCandidates: Candidate[] = [
 ];
 
 export const pasayCityMayorCandidates: Candidate[] = [
-  { id: "pasay-m-1", name: "CALIXTO-RUBIANO, EMI (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-m-2", name: "MARASIGAN, WAYNE", party: "INDEPENDENT" },
+  { id: "pasay-m-1", name: "CALIXTO-RUBIANO, EMI (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-m-2", name: "MANGUERRA, WOWEE (AKSYON)", party: "AKSYON DEMOKRATIKO" },
 ];
 
 export const pasayCityViceMayorCandidates: Candidate[] = [
   { id: "pasay-vm-1", name: "CALIXTO, MARK (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-vm-2", name: "TOLENTINO, CHUCHODINO (IND)", party: "INDEPENDENT" },
+  { id: "pasay-vm-2", name: "TOLENTINO, OKIDOCBONG (IND)", party: "INDEPENDENT" },
 ];
 
 // First district council candidates from image 1
 export const pasayCityCouncilorFirstCandidates: Candidate[] = [
-  { id: "pasay-c1-1", name: "ADVINCULA, JHAZ (PPP)", party: "PARTIDO PARA SA PILIPINO" },
-  { id: "pasay-c1-2", name: "CALIMBAHIN, ACE (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c1-3", name: "PECHIE, MARLON (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c1-4", name: "SATO, JOCELYN (POPUBN)", party: "POPUBN" },
-  { id: "pasay-c1-5", name: "CUNETA, ROMAY (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c1-6", name: "DEL ROSARIO, GRACE (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c1-7", name: "ROXAS, SHARON (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c1-8", name: "ALVINA, ARNET (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c1-9", name: "GABRIEL, TIGER (POPUBN)", party: "POPUBN" },
-  { id: "pasay-c1-10", name: "SANTOS, POBLET (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c1-11", name: "SANTOS, TINO CLAUDIO (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c1-12", name: "ANDERSON, RICHARD (PPP)", party: "PARTIDO PARA SA PILIPINO" },
+  { id: "pasay-c1-1", name: "ADVINCULA, JHAZ (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-c1-2", name: "ADVINCULA, RONJAY (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c1-3", name: "ALVINA, AMBET (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-4", name: "ANDERSON, RICHARD  (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c1-5", name: "CALUBAYAN, ACE (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c1-6", name: "CUNETA, TONYA TONYO (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-7", name: "DEL ROSARIO, BOYET DING(LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-8", name: "GABRIEL, TIGER  (PDPLBN)", party: "PARTIDO DEMOKRATIKO PILIPINO LAKAS NG BAYAN" },
+  { id: "pasay-c1-9", name: "PESEBRE, MARLON (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-10", name: "SANTOS, GRACE (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-11", name: "SANTOS, PUCHET (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c1-12", name: "SANTOS, TINO CLAUDIO (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c1-13", name: "SATO, JOCELYN  (PDPLBN)", party: "PARTIDO DEMOKRATIKO PILIPINO LAKAS NG BAYAN" },
+  { id: "pasay-c1-14", name: "STA. MARIA, RAMON  (KBL)", party: "KILUSANG BAGONG LIPUNAN" },
 ];
 
 // Second district council candidates from image 2
 export const pasayCityCouncilorSecondCandidates: Candidate[] = [
-  { id: "pasay-c2-1", name: "ANCOL, ALLO-MOH (PPP)", party: "PARTIDO PARA SA PILIPINO" },
-  { id: "pasay-c2-2", name: "DEL ROSARIO, BOYET ARTY (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c2-3", name: "PANITA, DANNY (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c2-4", name: "YARUT, RAMON RY (LP)", party: "LIBERAL PARTY" },
-  { id: "pasay-c2-5", name: "BAJUYO, FANTA (POPUBN)", party: "POPUBN" },
-  { id: "pasay-c2-6", name: "MANINGAT, JESSJ QUEZON (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c2-7", name: "RUBIANO, CALIXTO LUIGI (LAKAS)", party: "LAKAS-CMD" },
-  { id: "pasay-c2-8", name: "BONGAT, HECTOR (IND)", party: "INDEPENDENT" },
-  { id: "pasay-c2-9", name: "MAGAT, INEN (PPP)", party: "PARTIDO PARA SA PILIPINO" },
-  { id: "pasay-c2-10", name: "SO, ATE BABY (AKSYON)", party: "AKSYON DEMOKRATIKO" },
-  { id: "pasay-c2-11", name: "CADUDOY, BONIELO (PPP)", party: "PARTIDO PARA SA PILIPINO" },
-  { id: "pasay-c2-12", name: "PANALIGAN, ALLAN (PPP)", party: "PARTIDO PARA SA PILIPINO" },
-  { id: "pasay-c2-13", name: "VENDIVEL, IAN (PPP)", party: "PARTIDO PARA SA PILIPINO" },
+  { id: "pasay-c2-1", name: "ARCEO, ALLO-MOTI (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-c2-2", name: "BAJUYO, FANTA (PDPLBN)", party: "PARTIDO DEMOKRATIKO PILIPINO LAKAS NG BAYAN" },
+  { id: "pasay-c2-3", name: "BONGAT, HECTOR (PDPLBN)", party: "PARTIDO DEMOKRATIKO PILIPINO LAKAS NG BAYAN" },
+  { id: "pasay-c2-4", name: "CADUCOY, RONELO (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c2-5", name: "DEL ROSARIO, BOYET YUYU (TAPAT)", party: "TAPAT" },
+  { id: "pasay-c2-6", name: "JIMENEZ, PAPSY QUEZON (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c2-7", name: "MAGAT, KHEN (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-c2-8", name: "PANALIGAN, ALLAN (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-c2-9", name: "PANTIA, DANNY (IND)", party: "INDEPENDENT" },
+  { id: "pasay-c2-10", name: "RUBIANO, CALIXTO LUIGI (LAKAS)", party: "LAKAS-CMD" },
+  { id: "pasay-c2-11", name: "SO, ATE BABY (AKSYON)", party: "AKSYON DEMOKRATIKO" },
+  { id: "pasay-c2-12", name: "VENDIVEL, IAN (PFP)", party: "PARTIDO FEDERAL NG PILIPINAS" },
+  { id: "pasay-c2-13", name: "YABUT, RAMON RY (LP)", party: "LIBERAL PARTY" },
 ];
